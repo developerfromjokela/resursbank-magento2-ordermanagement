f1121806fb1604e076b118f057a76d6c3881a185
Marcus Gullberg <marcus.gullberg@resurs.se>
2020-09-08 14:08:19 +0200

    Added initial scaffhold for new module

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Resursbank_Ordermanagement',
+    __DIR__
+);
