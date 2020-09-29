39b540bcb48b14f106f1b8c02d8aff67ccafb0cc
Marcus Gullberg <marcus.gullberg@resurs.se>
2020-09-29 10:46:44 +0200

    Listing the callbacks

---
+++ Block/Adminhtml/System/Config/Callback/Listing.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ordermanagement\Block\Adminhtml\System\Config\Callback;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Resursbank\Ordermanagement\Helper\Callback as CallbackHelper;
+
+/**
+ * @package Resursbank\Ordermanagement\Block\Adminhtml\System\Config\Callback
+ */
+class Listing extends Field
+{
+    /**
+     * @var CallbackHelper
+     */
+    public $callbackHelper;
+
+    /**
+     * @param Context $context
+     * @param CallbackHelper $callbackHelper
+     */
+    public function __construct(
+        Context $context,
+        CallbackHelper $callbackHelper
+    ) {
+        $this->setTemplate('system/config/callback/listing.phtml');
+        $this->callbackHelper = $callbackHelper;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * Unset some non-related element parameters.
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element): string
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+
+        return parent::render($element);
+    }
+
+    /**
+     * Get the button and scripts contents
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element): string
+    {
+        return $this->_toHtml();
+    }
+}
