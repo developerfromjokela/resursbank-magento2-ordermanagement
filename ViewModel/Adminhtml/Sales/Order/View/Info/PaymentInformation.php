b957a3a94156b2fe8f2c0f9f2f48b8f3e1b1b7cd
Peter Sjöqvist <info@mageparts.com>
2021-03-03 17:32:06 +0100

    M2OM-73
    Moved StopConfirmationEmail from Core module.

--- ViewModel/Adminhtml/Sales/Order/View/Info/PaymentInformation.php
+++ ViewModel/Adminhtml/Sales/Order/View/Info/PaymentInformation.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 
 namespace Resursbank\Ordermanagement\ViewModel\Adminhtml\Sales\Order\View\Info;
 
-use Exception;
 use Magento\Checkout\Helper\Data;
 use Magento\Framework\Exception\ValidatorException;
 use Magento\Framework\Pricing\PriceCurrencyInterface;
