e0072605dea993f11aee56184a3503a283042c6a
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-03-23 13:45:34 +0100

    M2OM-148 Add test for callback HTTP status code for wrong paymentId

--- Test/Api/Model/CallbackQueueTest.php
+++ Test/Api/Model/CallbackQueueTest.php
@@ -8,12 +8,68 @@ declare(strict_types=1);
 
 namespace Resursbank\Ordermanagement\Test\Api\Model;
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Authentication\OauthHelper;
+use Magento\TestFramework\Authentication\Rest\OauthClient;
+use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\WebapiAbstract;
 use Magento\Framework\Webapi\Rest\Request;
 use Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use Resursbank\Ordermanagement\Helper\Callback as CallbackHelper;
 
 class CallbackQueueTest extends WebapiAbstract
 {
+    /** @var CallbackHelper|MockObject  */
+    private CallbackHelper|MockObject $callbackHelper;
+
+    protected function setUp(): void
+    {
+        $this->callbackHelper = $this->createMock(CallbackHelper::class);
+    }
+
+    /**
+     * Assert that we get correct HTTP response code for missing orders
+     *
+     * @return void
+     * @throws LocalizedException
+     * @throws \Magento\Framework\Oauth\Exception
+     */
+    public function testMissingOrder(): void
+    {
+        // Generate URL
+        $paymentId = "foobar1234";
+        $digest = strtoupper(sha1($paymentId.$this->callbackHelper->salt()));
+        $resourcePath = '/V1/resursbank_ordermanagement/order/update/paymentId/'.$paymentId.'/digest/'.$digest;
+        $storeCode = Bootstrap::getObjectManager()
+            ->get(StoreManagerInterface::class)
+            ->getStore()
+            ->getCode();
+        $url = rtrim(TESTS_BASE_URL, '/').'/rest/'.$storeCode.'/'.ltrim($resourcePath, '/');
+
+        // Auth stuff, a bit unclear if this is actually needed in testing
+        $accessCredentials = OauthHelper::getApiAccessCredentials();
+        /** @var OauthClient $oAuthClient */
+        $oAuthClient = $accessCredentials['oauth_client'];
+        $authHeader = $oAuthClient->buildOauthAuthorizationHeader(
+            $url,
+            $accessCredentials['key'],
+            $accessCredentials['secret'],
+            []
+        );
+        $authHeader = array_merge($authHeader, ['Accept: application/json', 'Content-Type: application/json']);
+
+        // Call API
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $authHeader);
+        curl_exec($ch);
+        $reqInfo = curl_getinfo($ch);
+        $httpStatusCode = $reqInfo['http_code'];
+        $this->assertEquals(410, $httpStatusCode);
+    }
+
     /**
      * Assert that the test endpoint exists
      *
